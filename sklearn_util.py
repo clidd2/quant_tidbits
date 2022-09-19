67d35f8d5a6a0775f0a899bc12e48866e0f0e24c
clidd2 <31219768+clidd2@users.noreply.github.com>
2022-09-19 08:53:16 -0400

    Adding some comments while its still fresh in my mind

--- sklearn_util.py
+++ sklearn_util.py
@@ -16,12 +16,13 @@ import numpy as np
 class BaseProcessor(BaseEstimator, TransformerMixin):
     def __init__(self):
     return None
-    
+
   def fit(self, X=None, y=None):
     return self
 
   def transform(self, X=None):
     return self
+
 
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
@@ -124,12 +125,27 @@ class CustomModelTrain:
 
 
     def generate_feature_union(self):
+        '''
+        Function selects numeric and categorical columns, adds selection to each
+        respective pipeline, and creates a master preprocessing pipeline
+
+        params
+        ======
+        None
+
+        returns
+        =======
+        FeatureUnion object containing num/cat columns as transformer pipeline
+        '''
+
+        #checks if num/cat columns explicitly stated, finds them otherwise
         num_cols = self._explicit_num_cols if len(self._explicit_num_cols) > 0 \
         else self.select_numeric_cols()
 
         cat_cols = self._explicit_cat_cols if len(self._explicit_cat_cols) > 0 \
         else self.select_categorical_cols()
 
+        #insert generic dataframe selector into each respective pipeline
         self._num_pipe.steps.insert(0,['selector',DataFrameSelector(num_cols)])
         self._cat_pipe.steps.insert(0,['selector',DataFrameSelector(cat_cols)])
 
@@ -140,15 +156,37 @@ class CustomModelTrain:
 
 
     def full_pipeline(self):
+        '''
+        Selects data, splits into training/test sets, processes data, and
+        trains model
+
+        params
+        ======
+        None
+
+        returns
+        =======
+        model (pipeline model): sklearn pipeline which transforms data and
+                                generates a useable model
+        X_test (pd.DataFrame): df containing testing parameter data
+        y_test (pd.DataFrame): df containing testing target data
+        '''
+
+        #generate desired params/target dataframes
         X = self._df[self._params]
         y = self._df[self._target]
+
+        #use provided test split for data
         X_train, X_test, y_train, y_test = train_test_split(X, y, \
                                                     test_size = self._test_set)
 
+        #pipeline and model together to ensure all data matches accordingly
         full_pipeline = Pipeline(steps=[
         ('preprocess_pipeline', self._preprocess_pipe   ),
         ('model', self._model)])
 
+
+        #fit model for testing purposes
         model = full_pipeline.fit(X_train, y_train)
         score = model.score(X_test, y_test)
-        return model
+        return model, X_test, y_test
